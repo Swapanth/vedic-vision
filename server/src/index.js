6d043c0c1f71f3891a39b939b99c85fd9568cd65
Swapanth <swapanthvakapalli@gmail.com>
2025-08-03 17:51:34 +0530

    pushes

--- server/src/index.js
+++ server/src/index.js
@@ -52,7 +52,8 @@ const corsOptions = {
             'http://localhost:5173',
             'http://localhost:5174',
             'http://localhost:3000',
-            'http://localhost:3001'
+            'http://localhost:3001',
+            'https://vedic-vision-app.vercel.app'
         ];
         
         if (allowedOrigins.indexOf(origin) !== -1) {
